e0308e22fef57871e9fcfb0c949e3cca99a27bef
GHysmu <yus60401@gmail.com>
2023-02-14 14:01:31 -0600

    change gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # Define your application routes per the DSL in https://guides.rubyonrails.org  resources :movies
+  root :to => redirect('/movies')
   # Defines the root path route ("/")
   # root "articles#index"
 end
